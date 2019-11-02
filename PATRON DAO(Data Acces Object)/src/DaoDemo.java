c3be94db848134cd8cec2cadd1269269f4f8c5c8
Francisco Alanzaves Romero <57249489+FranciscoAlanzavesRomero@users.noreply.github.com>
2019-11-02 18:40:45 +0100

    Add files via upload

---
+++ PATRON DAO(Data Acces Object)/src/DaoDemo.java
@@ -0,0 +1,24 @@
+import DAO.ClienteDaoImpl;
+import IDAO.IClienteDao;
+import Modelo.Cliente;
+
+public class DaoDemo {
+ 
+	public static void main(String[] args) {
+		// objeto para manipular el dao
+		IClienteDao clienteDao = new ClienteDaoImpl();
+ 
+		// imprimir los clientes
+		clienteDao.obtenerClientes().forEach(System.out::println);
+ 
+		// obtner un cliente
+		Cliente cliente = clienteDao.obtenerCliente(0);
+		cliente.setApellido("Pardo");
+		//actualizar cliente
+		clienteDao.actualizarCliente(cliente);
+ 
+		// imprimir los clientes
+		System.out.println("*****");
+		clienteDao.obtenerClientes().forEach(System.out::println);
+	}
+}
